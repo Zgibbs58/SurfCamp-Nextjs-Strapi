18bccd3ff344cc4116667d21a183edd53c7eb5fd
Zach Gibbs <zacharywgibbs@gmail.com>
2024-04-11 18:05:26 -0500

    Set up project by installing next and strapi. Then set up sass style sheets and creating header.

---
+++ surfcamp_frontend/app/components/Header.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  const navItems = [
+    { href: "/", label: "the camp." },
+    { href: "/experience", label: "the experience." },
+    { href: "/blog", label: "the blog." },
+  ];
+
+  return (
+    <header className="header">
+      <Image className="header_logo" width={100} height={100} alt="logo" src={"/assets/logo.svg"}></Image>
+      <ul className="header_nav">
+        {navItems.map((item) => (
+          <li key={item.href}>
+            <Link href={item.href}>
+              <h5>{item.label}</h5>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <button className="btn btn--black btn--small">BOOK NOW</button>
+    </header>
+  );
+};
+
+export default Header;
